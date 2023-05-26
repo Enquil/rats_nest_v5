c96a4ee3249fed97bdd49ce9587fe076a1073d48
Enquil <jim.e.olesen@gmail.com>
2023-05-26 09:38:59 +0000

    added home and products urls and a view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from django.views import generic
+from django.contrib import messages
 
-# Create your views here.
+
+class HomeView(View):
+
+    def get(self, request, *args, **kwargs):
+        template = 'home/index.html'
+        return render(request, template)

cd7f702495beff9df993b3c7d8c5adbb3386d964
Enquil <jim.e.olesen@gmail.com>
2023-05-26 15:01:26 +0000

    added search functionality

--- products/views.py
+++ products/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
-from django.views import View
-from django.views import generic
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Category
+from django.views import View
 
 
 class ProductView(View):
 
     def get(self, request, *args, **kwargs):
         template = 'products/products.html'
+        if 'q' in request.GET:
+            query = request.GET['q']
         return render(request, template)

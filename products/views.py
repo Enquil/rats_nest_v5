4b6cbe1cdd136510906e1e2a3b350353eadc9127
Enquil <jim.e.olesen@gmail.com>
2023-05-30 14:25:40 +0000

    added product object to view

--- products/views.py
+++ products/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .models import Category, Brand
+from .models import Category, Brand, Product
 from django.views import View
 
 
 class ProductView(View):
 
     def get(self, request, query, *args, **kwargs):
+        products = Product.objects.all().order_by('-price')
+        print(products)
         if query == 'search-query':
             meow = request.GET['q']
             print(meow)
         template = 'products/products.html'
-        return render(request, template)
+        return render(request, template,
+                      {"products": products})

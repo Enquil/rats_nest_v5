437038b1c92cec855b84ec8067d233bee8dc0ff3
Enquil <jim.e.olesen@gmail.com>
2023-06-02 09:39:07 +0000

    added logic to display all categories with the same parent in products.html

--- products/views.py
+++ products/views.py
@@ -11,22 +11,32 @@ class ProductView(View):
     def get(self, request, *args, **kwargs):
 
         products = Product.objects.all().order_by('-price')
+        categories = Category.objects.all()
+        parent = None
+        category_nav = None
 
         if 'category' in request.GET:
-            category = int(request.GET['category'])
-            if category <= 3:
+            category_pk = int(request.GET['category'])
+            category = Category.objects.filter(pk=category_pk)
+            if category_pk <= 3:
+                categories = Category.objects.filter(parent=category_pk)
                 products = Product.objects.filter(
-                            domain=category).order_by('-price')
+                            domain=category_pk).order_by('-price')
+                category_nav = [category]
             else:
-                products = Product.objects.filter(
-                            category=category).order_by('-price')
+                parent = Category.objects.filter(pk=category[0].parent.pk)
+                categories = Category.objects.filter(parent=category[0].parent)
+                category_nav = [parent, category]
 
         if 'q' in request.GET:
             q = request.GET['q']
 
         return render(request, 'products/products.html',
                       {
-                        'products': products
+                        'products': products,
+                        'categories': categories,
+                        'category_nav': category_nav,
+                        'parent': parent,
                         })
 
 

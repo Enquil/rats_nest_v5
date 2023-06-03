6a6dc43bb6b82c7e3a9396354925b62a146492b6
Enquil <jim.e.olesen@gmail.com>
2023-06-03 09:52:49 +0000

    formatted code

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductView.as_view(), name='products'),
-    path('<product_sku>', views.ProductDetail.as_view(), name='product_detail'),
+    path('<product_sku>', views.ProductDetail.as_view(),
+         name='product_detail'),
 ]

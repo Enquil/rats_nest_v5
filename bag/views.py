dc08e96d14a3a2adc6290feabad7f78057a6584b
Enquil <jim.e.olesen@gmail.com>
2023-06-03 06:42:21 +0000

    added basic view for bag, created urls.py

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib import messages
 
-# Create your views here.
+
+class BagView(View):
+    '''
+    Bag View
+    '''
+    def get(self, request, *args, **kwargs):
+        template = 'bag/bag.html'
+        return render(request, template)

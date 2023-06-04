905117906b96e37e5cdd03aeaa32ebae7b184b69
Enquil <jim.e.olesen@gmail.com>
2023-06-04 15:05:59 +0000

    added if statement to handle if delivery_treshold has been met, added context dict

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,3 +1,8 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def bag_contents(request):
 
@@ -5,5 +10,23 @@ def bag_contents(request):
     total = 0
     product_count = 0
 
-    context = {}
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
+
     return context

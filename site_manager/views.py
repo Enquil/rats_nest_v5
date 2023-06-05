2ee379fb3dec0f346459d678ba1a1b4742037e2d
Enquil <jim.e.olesen@gmail.com>
2023-06-05 12:52:23 +0000

    added def post block to SiteManager

--- site_manager/views.py
+++ site_manager/views.py
@@ -5,6 +5,8 @@ from django.db.models.functions import Lower
 from products.models import Category, Brand, Product, Domain
 from django.views import View
 from .forms import AddProductForm
+from django.http import (HttpResponse,
+                         HttpResponseRedirect)
 
 
 class SiteManager(View):
@@ -12,5 +14,16 @@ class SiteManager(View):
     def get(self, request, *args, **kwargs):
 
         return render(request, 'site_manager/site_manager.html', {
-            'add_form': AddProductForm(),
-        })
+            'add_product_form': AddProductForm(),
+            })
+
+    def post(self, request, *args, **kwargs):
+
+        add_product_form = AddProductForm(request.POST, request.FILES)
+
+        if add_product_form.is_valid():
+            form = add_product_form.save(commit=False)
+            form.save()
+            return render(request, ('site_manager/site_manager.html'))
+        else:
+            add_product_form = AddProductForm()

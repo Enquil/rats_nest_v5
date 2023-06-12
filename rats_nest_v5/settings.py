0e1566e9517612a934f6c4c30d1744c52111a75e
Enquil <jim.e.olesen@gmail.com>
2023-06-12 08:49:47 +0000

    removed secret key, added DEV setting for DEBUG in os.envron

--- rats_nest_v5/settings.py
+++ rats_nest_v5/settings.py
@@ -26,10 +26,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ['rats-nest-v2.herokuapp.com',
                  '8000-enquil-ratsnestv5-c4dp1j1u7o0.ws-eu99.gitpod.io']
